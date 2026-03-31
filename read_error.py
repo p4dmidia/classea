0262da47bcace9732844aa766151c39df6f0df39
P4D Midia <agenciap4d@gmail.com>
2026-03-31 18:15:43 -0300

    versão correção erro git hub 2

--- read_error.py
+++
@@ -1,5 +0,0 @@
-import os
-
-with open("c:\\Users\\eu\\Documents\\P4D\\Projetos\\Classe A\\build_error.txt", "rb") as f:
-    content = f.read().decode("utf-16-le")
-    print(content[:2000]) # Print first 2000 characters
--- read_errors.py
+++
@@ -1,6 +0,0 @@
-import os
-
-with open("c:\\Users\\eu\\Documents\\P4D\\Projetos\\Classe A\\ts_errors.txt", "rb") as f:
-    # Try decoding as utf-16-le
-    content = f.read().decode("utf-16-le")
-    print(content)
